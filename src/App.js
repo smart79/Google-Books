bb5f870fbcc9d2db425f69b50cf4e34ecbd0cccd
Chris Martinez <smartinez79@protonmail.com>
2019-08-03 18:37:56 -0500

    update to table in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Table } from 'reactstrap';
 
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
 
-function App() {
-  return (
-    <div className="App container">
-      <table></table>
-    </div>
-  );
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Book Title</td>
+
+              <td>2.5</td>
+
+              <td>
+
+                <button color="success" size="sm" className="mr-2">Edit</button>
+
+                <button color="danger" size="sm" className="mr-2">Delete</button>
+
+              </td>
+
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
-
 export default App;
